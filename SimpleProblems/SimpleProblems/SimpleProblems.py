752ae0491a7fd749d3828e19feb2a24e0a45352b
Gray <mganthon@hotmail.com>
2016-11-28 06:04:13 -0500

    implemented compute sum and products with user choice of computation

--- SimpleProblems/SimpleProblems/SimpleProblems.py
+++ SimpleProblems/SimpleProblems/SimpleProblems.py
@@ -1,6 +1,18 @@
 n = input("Enter a number.\n--> ")
-sum = 0
-for i in range(1,n+1):
-    if (i % 5 == 0 or i % 3 == 0) : 
-        sum += i 
-print("sum: " + str(sum))
+while True :    
+    print("Would you like to compute the sum or poduct?\n")
+    option = raw_input("(S\P) -->").upper()
+    if option == "S" or "P" :
+        break
+    print("unknon option:\n")
+
+if option == "S" :
+    sum = 0
+    for i in range(1,n+1):
+            sum += i 
+    print("sum: " + str(sum))
+elif option == "P" :
+    product = 1
+    for i in range(1,n+1):
+        product *= i 
+    print("product: " + str(product))
